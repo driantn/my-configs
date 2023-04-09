9346b51e99c1322059b1e48dc340e4b68e4f1d8a
Driant Naun <driantn@gmail.com>
2023-04-09 21:43:37 +0200

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: "localhost",
-		port: 3000,
-	},
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
 });
